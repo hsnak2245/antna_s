8a2491524dcf919a0aa0523bbff4032f1089c2c4
hsnak2245 <140240196+hsnak2245@users.noreply.github.com>
2025-01-28 17:42:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.7/+esm'
 
 const SUPABASE_CONFIG = {
-    url: 'https://phituvbneyyjtixweeqq.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoaXR1dmJuZXl5anRpeHdlZXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5ODU5NDEsImV4cCI6MjA1MzU2MTk0MX0.wTrrg3pz0Rl-L4t7pyJuva5q4VPdnvX1rBMP-xpnKpU',
+    url: import.meta.env.VITE_SUPABASE_URL,
+    key: import.meta.env.VITE_SUPABASE_ANON_KEY,
     tables: {
         alerts: 'alerts',
         centers: 'centers',
